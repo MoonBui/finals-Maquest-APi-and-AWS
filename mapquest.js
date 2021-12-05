398dfa424e9ae3558d670f1ff9bb2eb18c337bd9
Moon Bui <buinm@miamioh.edu>
2021-12-05 16:06:22 -0500

    second update to show results

--- mapquest.js
+++ mapquest.js
@@ -23,14 +23,14 @@ function submitDirections() {
         var maneuvers = leg.maneuvers;
 
         $("#result").html("");
-        $("#result").append("<thead><tr>" + "th class='col'>Narratives</th>" +
+        $("#result").append("<br> <thead><tr>" + "th class='col'>Narratives</th>" +
             "<th class='col'>Distance</th>" + "<th class='col'>Time</th>" +
-            "< class='col' >Thumbnail</th></tr></thead><tbody>")
+            "<th class='col'>Thumbnail</th></tr></thead><tbody>")
 
         for (let i = 0; i < maneuvers.length - 1; i++) {
             $("#result").append("<tr><td> " + maneuvers[i].narrative + "</td><td>" +
                 maneuvers[i].distance + "</td><td>" + maneuvers[i].time + "</td><td>" +
-                "img class='img-result' src='" + maneuvers[i].mapURL + "'width='250' height='auto'" + "</td></tr>");
+                "<img class='img-result' src='" + maneuvers[i].mapURL + "'width='250' height='auto'>" + "</td></tr>");
         }
 
         $("#result").append("<tr><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr></tbody>");
@@ -38,7 +38,7 @@ function submitDirections() {
         var LatLng = route.boundingBox.lr.lat + "," + route.boundingBox.lr.lng + "," + route.boundingBox.ul.lat + "," + route.boundingBox.ul.lng;
         var chart = chartURL + key + "&shapeFormat=raw&width=425&height=350&latLngCollection=" + LatLng;
 
-        $("#result").append("<h1 class='elevation-chart'>Elevation Chart</h1>");
+        $("#result").append("<br> <h1 class='elevation-chart'>Elevation Chart</h1>");
         $("#result").append("<img src='" + chart + "' width='400' height='300'>");
 
         console.log(data);
