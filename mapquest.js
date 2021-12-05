85f05b52a9372e78fb3ef466fb4095703162cb1e
EC2 Default User <ec2-user@ip-172-31-95-120.ec2.internal>
2021-12-05 04:50:03 +0000

    update

--- mapquest.js
+++ mapquest.js
@@ -1,42 +1,28 @@
-var URL="http://www.mapquestapi.com/directions/v2/route";
+var URL="http://www.mapquestapi.com/directions/v2/route?key=";
 var key="6wGZkKsmoHlB0C42ujYQGAD2EKtYWyBA";
 
-// var from = { "location": {"street": "301 East High St", "city" : "Oxford", "state": "OH"} };
-// var to = { "location": {"street": "601 Maple St", "city" : "Oxford", "state": "OH"} };
-var locations = { "locations": [ document.getElementById("location"), document.getElementById("destination") ], "options": {
-        "avoids": [],
-        "avoidTimedConditions": false,
-        "doReverseGeocode": true,
-        "shapeFormat": "raw",
-        "generalize": 0,
-        "routeType": "fastest",
-        "timeType": 1,
-        "locale": "en_US",
-        "unit": "m",
-        "enhancedNarrative": false,
-        "drivingStyle": 2,
-        "highwayEfficiency": 21.0
-    }
-}
+// submitDirections();
 
 function submitDirections(){
+var from = document.getElementById("location");
+var to = document.getElementById("destination");
+        var locations = { "from": document.getElementById("location").value, "to": document.getElementById("destination").value }
+	console.log(JSON.stringify(locations));
 	a=$.ajax({
-	url: URL,
-	method: "POST",
-	dataType: 'json',
-	contentType: 'json',
-	data: JSON.stringify(locations),
+	url: `${URL}${key}&from=${encodeURIComponent(from.value)}&to=${encodeURIComponent(to.value)}`,
+	method: "GET",
+	// data: JSON.stringify(locations)
+	}).done(function(data) {
+		$("#result").append(locations);
+		$("#result").append(data);
+	 	console.log(locations);
+		console.log(data);
 
-	success: function(data) {
-	$("#result").append(data);
-	log(data);
-  },
-	error: function(data) {
-	$("#result").append("error " + data);
-	log("error " + data);
-  }
-
-  });
+		var route = data.route;
+		var legs = route.legs;
+    }).fail(function(error) {
+		$("#result").append("error");
+    });
 }
 
 
