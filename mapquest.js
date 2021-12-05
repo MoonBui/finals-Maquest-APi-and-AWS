937c3f2e7a6598ba8144e3a0c8f0ea07e59414eb
EC2 Default User <ec2-user@ip-172-31-95-120.ec2.internal>
2021-12-05 00:21:50 +0000

    update 3rd time with UI change

--- mapquest.js
+++ mapquest.js
@@ -3,7 +3,7 @@ var key="6wGZkKsmoHlB0C42ujYQGAD2EKtYWyBA";
 
 // var from = { "location": {"street": "301 East High St", "city" : "Oxford", "state": "OH"} };
 // var to = { "location": {"street": "601 Maple St", "city" : "Oxford", "state": "OH"} };
-var locations = { "locations": ["501 e high street oxford ohio 45056", "7847 VOA Park Dr, West Chester Township, OH 45069"], "options": {
+var locations = { "locations": [ document.getElementById("location"), document.getElementById("destination") ], "options": {
         "avoids": [],
         "avoidTimedConditions": false,
         "doReverseGeocode": true,
@@ -19,16 +19,23 @@ var locations = { "locations": ["501 e high street oxford ohio 45056", "7847 VOA
     }
 }
 
-function directions(){
+function submitDirections(){
 	a=$.ajax({
 	url: URL,
 	method: "POST",
 	dataType: 'json',
 	contentType: 'json',
-	data: JSON.stringify(locations)
-  }).success(function(data) {
+	data: JSON.stringify(locations),
+
+	success: function(data) {
 	$("#result").append(data);
 	log(data);
+  },
+	error: function(data) {
+	$("#result").append("error " + data);
+	log("error " + data);
+  }
+
   });
 }
 
