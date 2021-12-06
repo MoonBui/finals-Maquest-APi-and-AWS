b4a57bf49a2ec99a4a7ae31e521f83d2dc8ce5d6
Moon Bui <buinm@miamioh.edu>
2021-12-06 17:02:19 -0500

    changes to function

--- mapquest.js
+++ mapquest.js
@@ -133,36 +133,32 @@ function requestData() {
         // Append overview table of results
         $("#result").append(resultTable);
 
-        // Function to show details results when button is clicked
-        function showResults(x) {
-            var sth = document.getElementById("detail-result");
-            sth.html("");
-            var maneuvers = JSON.parse(results[x].value.maneuvers);
-            var detailTable = "";
 
-            for (let i = 0; i < maneuvers.length - 1; i++) {
-                // $("#result").append("<tr><td> " + maneuvers[i].narrative + "</td><td>" +
-                //     maneuvers[i].distance + "</td><td>" + maneuvers[i].time + "</td><td>" +
-                //     "<img class='img-result' src='" + maneuvers[i].mapURL + "'width='250' height='auto'>" + "</td></tr>");
-                detailTable += "<tr><td> " + maneuvers[i].narrative + "</td><td class='center'>" +
-                    maneuvers[i].distance + "</td><td class='center'>" + maneuvers[i].time + "</td><td>" +
-                    "<img class='img-result' src='" + maneuvers[i].mapUrl + "'width='250' height='auto'>" + "</td></tr>";
-            }
-
-            // $("#result").append("<tr><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr>");
-            // $("#result").append("</tbody></table>");
-
-            detailTable += "<tr><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr>" + "</tbody></table>";
-            $("#detail-result").append(detailTable);
-
-            var LatLng = route.boundingBox.lr.lat + "," + route.boundingBox.lr.lng + "," + route.boundingBox.ul.lat + "," + route.boundingBox.ul.lng;
-            var chart = chartURL + key + "&shapeFormat=raw&width=425&height=350&latLngCollection=" + LatLng;
-
-            $("detail-result").append("<br> <h1 class='elevation-chart'>Elevation Chart</h1>");
-            $("detail-result").append("<img id='chart' src='" + chart + "' width='400' height='300'>");
-        }
 
     }).fail(function(error) {
 
     });
+
+    // Function to show details results when button is clicked
+    function showResults(x) {
+        var sth = document.getElementById("detail-result");
+        sth.html("");
+        var maneuvers = JSON.parse(x.value.maneuvers);
+        var detailTable = "";
+
+        for (let i = 0; i < maneuvers.length - 1; i++) {
+            detailTable += "<tr><td> " + maneuvers[i].narrative + "</td><td class='center'>" +
+                maneuvers[i].distance + "</td><td class='center'>" + maneuvers[i].time + "</td><td>" +
+                "<img class='img-result' src='" + maneuvers[i].mapUrl + "'width='250' height='auto'>" + "</td></tr>";
+        }
+
+        detailTable += "<tr><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr>" + "</tbody></table>";
+        $("#detail-result").append(detailTable);
+
+        var LatLng = route.boundingBox.lr.lat + "," + route.boundingBox.lr.lng + "," + route.boundingBox.ul.lat + "," + route.boundingBox.ul.lng;
+        var chart = chartURL + key + "&shapeFormat=raw&width=425&height=350&latLngCollection=" + LatLng;
+
+        $("detail-result").append("<br> <h1 class='elevation-chart'>Elevation Chart</h1>");
+        $("detail-result").append("<img id='chart' src='" + chart + "' width='400' height='300'>");
+    }
 }
