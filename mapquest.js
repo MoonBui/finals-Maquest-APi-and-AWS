83de57bcf348b8e21574bae8a4c0c8cd93404f85
Moon Bui <buinm@miamioh.edu>
2021-12-06 23:06:37 -0500

    test

--- mapquest.js
+++ mapquest.js
@@ -125,27 +125,28 @@ function requestData() {
                 i + "' aria-expanded='true'> Details </button></td>" + "</tr>";
 
             // Table that can collapse and show details when clicking button
-            // resultTable += "<div id='collapse" + i + "' class='collapse show'>";
-            // resultTable += "<br> <table class='border accordion-body'><thead class='thead'><tr id='collapse" + i + "'class='accordion-body collapse show' >" + "<td>Narratives</td>" +
-            //     "<td>Distance</td>" + "<td>Time</td>" +
-            //     "<td>Thumbnail</td></tr></thead><tbody>";
+            resultTable += "<div id='collapse" + i + "' class='collapse show accordion-body'>";
+            resultTable += "<br> <table class='border accordion-body'><thead class='thead'><tr id='collapse" + i + "'class='accordion-body collapse show' >" + "<td>Narratives</td>" +
+                "<td>Distance</td>" + "<td>Time</td>" +
+                "<td>Thumbnail</td></tr></thead><tbody>";
 
-            // for (let j = 0; j < maneuvers.length - 1; j++) {
-            //     resultTable += "<tr id='collapse" + i + "'class='accordion-body collapse show'><td> " + maneuvers[j].narrative + "</td><td class='center'>" +
-            //         maneuvers[j].distance + "</td><td class='center'>" + maneuvers[j].time + "</td><td>" +
-            //         "<img class='img-result' src='" + maneuvers[j].mapUrl + "'width='250' height='auto'>" + "</td></tr>";
-            //}
+            for (let j = 0; j < maneuvers.length - 1; j++) {
+                resultTable += "<tr id='collapse" + i + "'class='accordion-body collapse show'><td> " + maneuvers[j].narrative + "</td><td class='center'>" +
+                    maneuvers[j].distance + "</td><td class='center'>" + maneuvers[j].time + "</td><td>" +
+                    "<img class='img-result' src='" + maneuvers[j].mapUrl + "'width='250' height='auto'>" + "</td></tr>";
+            }
 
-            // Detail table
-            // resultTable += "<tr id='collapse" + i + "'class='accordion-body collapse show'><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr>" + "</tbody></table>";
-            // resultTable += "<br> <h1 class='elevation-chart'>Elevation Chart</h1>";
-            // resultTable += "<img class='accordion-body collapse show' id='chart' src='" + value.chart + "' width='400' height='300'></div>";
-
+            //Detail table
+            resultTable += "<tr id='collapse" + i + "'class='accordion-body collapse show'><td>" + maneuvers[maneuvers.length - 1].narrative + "</td></tr>" + "</tbody></table>";
+            resultTable += "<br> <h1 class='elevation-chart'>Elevation Chart</h1>";
+            resultTable += "<img class='accordion-body collapse show' id='chart' src='" + value.chart + "' width='400' height='300'></div>";
+            $("#result").append(resultTable);
         }
-        resultTable += "</tbody></table></div>";
+        //resultTable += "</tbody></table></div>";
 
         // Append ending for overview table of results
-        $("#result").append(resultTable);
+        //$("#result").append(resultTable);
+        $("#result").append("</tbody></table></div>");
 
 
 
